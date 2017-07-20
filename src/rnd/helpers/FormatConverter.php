87659bb7e0a7f07f398988ba124fdee403c9f24e
Marko Mikulic <mikulic.marko@spark.ba>
2017-07-20 23:58:45 +0200

    Added missing methods in rnd\i18n\Formatter

---
+++ src/rnd/helpers/FormatConverter.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author: Marko Mikulic
+ */
+
+namespace rnd\helpers;
+
+
+class FormatConverter extends BaseFormatConverter
+{
+}
--- src/rnd/i18n/Formatter.php
+++ src/rnd/i18n/Formatter.php
@@ -4,8 +4,10 @@ namespace rnd\i18n;
 use DateTime;
 use DateTimeInterface;
 use DateTimeZone;
+use IntlDateFormatter;
 use rnd\base\InvalidConfigException;
 use rnd\base\InvalidParamException;
+use rnd\helpers\FormatConverter;
 use rnd\helpers\Html;
 use NumberFormatter;
 use rnd\base\Object;
@@ -417,6 +419,75 @@ class Formatter extends Object
 
 	// date and time formats
 
+	/**
+	 * @param int|string|DateTime $value the value to be formatted. The following
+	 * types of value are supported:
+	 *
+	 * - an integer representing a UNIX timestamp
+	 * - a string that can be [parsed to create a DateTime object](http://php.net/manual/en/datetime.formats.php).
+	 *   The timestamp is assumed to be in [[defaultTimeZone]] unless a time zone is explicitly given.
+	 * - a PHP [DateTime](http://php.net/manual/en/class.datetime.php) object
+	 *
+	 * @param string $format the format used to convert the value into a date string.
+	 * @param string $type 'date', 'time', or 'datetime'.
+	 * @throws InvalidConfigException if the date format is invalid.
+	 * @return string the formatted result.
+	 */
+	private function formatDateTimeValue($value, $format, $type)
+	{
+		$timeZone = $this->timeZone;
+		// avoid time zone conversion for date-only and time-only values
+		if ($type === 'date' || $type === 'time') {
+			list($timestamp, $hasTimeInfo, $hasDateInfo) = $this->normalizeDatetimeValue($value, true);
+			if ($type === 'date' && !$hasTimeInfo || $type === 'time' && !$hasDateInfo) {
+				$timeZone = $this->defaultTimeZone;
+			}
+		} else {
+			$timestamp = $this->normalizeDatetimeValue($value);
+		}
+		if ($timestamp === null) {
+			return $this->nullDisplay;
+		}
+		// intl does not work with dates >=2038 or <=1901 on 32bit machines, fall back to PHP
+		$year = $timestamp->format('Y');
+		if ($this->_intlLoaded && !(PHP_INT_SIZE === 4 && ($year <= 1901 || $year >= 2038))) {
+			if (strncmp($format, 'php:', 4) === 0) {
+				$format = FormatConverter::convertDatePhpToIcu(substr($format, 4));
+			}
+			if (isset($this->_dateFormats[$format])) {
+				if ($type === 'date') {
+					$formatter = new IntlDateFormatter($this->locale, $this->_dateFormats[$format], IntlDateFormatter::NONE, $timeZone, $this->calendar);
+				} elseif ($type === 'time') {
+					$formatter = new IntlDateFormatter($this->locale, IntlDateFormatter::NONE, $this->_dateFormats[$format], $timeZone, $this->calendar);
+				} else {
+					$formatter = new IntlDateFormatter($this->locale, $this->_dateFormats[$format], $this->_dateFormats[$format], $timeZone, $this->calendar);
+				}
+			} else {
+				$formatter = new IntlDateFormatter($this->locale, IntlDateFormatter::NONE, IntlDateFormatter::NONE, $timeZone, $this->calendar, $format);
+			}
+			if ($formatter === null) {
+				throw new InvalidConfigException(intl_get_error_message());
+			}
+			// make IntlDateFormatter work with DateTimeImmutable
+			if ($timestamp instanceof \DateTimeImmutable) {
+				$timestamp = new DateTime($timestamp->format(DateTime::ISO8601), $timestamp->getTimezone());
+			}
+			return $formatter->format($timestamp);
+		}
+		if (strncmp($format, 'php:', 4) === 0) {
+			$format = substr($format, 4);
+		} else {
+			$format = FormatConverter::convertDateIcuToPhp($format, $type, $this->locale);
+		}
+		if ($timeZone != null) {
+			if ($timestamp instanceof \DateTimeImmutable) {
+				$timestamp = $timestamp->setTimezone(new DateTimeZone($timeZone));
+			} else {
+				$timestamp->setTimezone(new DateTimeZone($timeZone));
+			}
+		}
+		return $timestamp->format($format);
+	}
 
 	/**
 	 * Formats the value as a date.
@@ -864,6 +935,8 @@ class Formatter extends Object
 	 * @param array $options optional configuration for the number formatter. This parameter will be merged with [[numberFormatterOptions]].
 	 * @param array $textOptions optional configuration for the number formatter. This parameter will be merged with [[numberFormatterTextOptions]].
 	 * @throws InvalidParamException if the input value is not numeric or the formatting failed.
+	 *
+	 * @return mixed
 	 */
 	private function formatSizeNumber($value, $decimals, $options, $textOptions)
 	{
