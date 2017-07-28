41b9b648d18daef6679d3a870a0714246a6037c4
Marko Mikulic <mikulic.marko@spark.ba>
2017-07-28 11:30:39 +0200

    Don't need to force using arrays in css & js

--- src/rnd/base/AssetBundle.php
+++ src/rnd/base/AssetBundle.php
@@ -31,28 +31,50 @@ abstract class AssetBundle extends Object
 	public function publish()
 	{
 		foreach ( $this->css as $i => $cssarr ) {
-			$name = $cssarr['name'] ?? time();
-			$deps = $cssarr['deps'] ?? [];
-			$url  = $cssarr['url'];
+			$name = uniqid('css-');
+			if (is_array($cssarr)) {
+				$deps = $cssarr['deps'] ?? [];
+				$url  = $cssarr['url'];
 
-			if (Url::isRelative( $url )) {
-				wp_enqueue_style($name . '-css-' . $i, Rnd::getAlias('@themeurl/' . ltrim( $url, '/')), $deps, filemtime(Rnd::getAlias( '@themeroot/' . ltrim( $url, '/'))));
+				if (Url::isRelative( $url )) {
+					wp_enqueue_style($name . '-css-' . $i, Rnd::getAlias('@themeurl/' . ltrim( $url, '/')), $deps, filemtime(Rnd::getAlias( '@themeroot/' . ltrim( $url, '/'))));
+				} else {
+					wp_enqueue_style($name . '-css-' . $i, $url, $deps, null);
+				}
 			} else {
-				wp_enqueue_style($name . '-css-' . $i, $url, $deps, null);
+				$url = $cssarr;
+				$deps = [];
+				if (Url::isRelative( $url )) {
+					wp_enqueue_style($name . '-css-' . $i, Rnd::getAlias('@themeurl/' . ltrim( $url, '/')), $deps, filemtime(Rnd::getAlias( '@themeroot/' . ltrim( $url, '/'))));
+				} else {
+					wp_enqueue_style($name . '-css-' . $i, $url, $deps, null);
+				}
 			}
 		}
 
 		foreach ( $this->js as $j => $jsarr ) {
-			$name = $jsarr['name'];
-			$deps = $jsarr['deps'] ?? [];
-			$url  = $jsarr['url'];
-			$in_footer = $jsarr['in_footer'] ?? true;
+			$name = uniqid('js-');
+			if (is_array($jsarr)) {
+				$deps = $jsarr['deps'] ?? [];
+				$url  = $jsarr['url'];
+				$in_footer = $jsarr['in_footer'] ?? true;
 
-			if (Url::isRelative( $url )) {
-				wp_enqueue_script($name . '-js-' . $j, Rnd::getAlias('@themeurl/' . ltrim( $url, '/')), $deps, filemtime(Rnd::getAlias( '@themeroot/' . ltrim( $url, '/'))), $in_footer);
+				if (Url::isRelative( $url )) {
+					wp_enqueue_script($name . '-js-' . $j, Rnd::getAlias('@themeurl/' . ltrim( $url, '/')), $deps, filemtime(Rnd::getAlias( '@themeroot/' . ltrim( $url, '/'))), $in_footer);
+				} else {
+					wp_enqueue_script($name . '-js-' . $j, $url, $deps, null, $in_footer);
+				}
 			} else {
-				wp_enqueue_script($name . '-js-' . $j, $url, $deps, null, $in_footer);
+				$url = $jsarr;
+				$in_footer = true;
+				$deps = [];
+				if (Url::isRelative( $url )) {
+					wp_enqueue_script($name . '-js-' . $j, Rnd::getAlias('@themeurl/' . ltrim( $url, '/')), $deps, filemtime(Rnd::getAlias( '@themeroot/' . ltrim( $url, '/'))), $in_footer);
+				} else {
+					wp_enqueue_script($name . '-js-' . $j, $url, $deps, null, $in_footer);
+				}
 			}
+
 		}
 	}
 
