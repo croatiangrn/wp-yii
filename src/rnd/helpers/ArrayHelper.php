1293aee738d89100cd4678b9f8cf23ef464e1a4e
Marko Mikulic <mikulic.marko@spark.ba>
2017-08-08 12:49:00 +0200

    Added map method

--- src/rnd/helpers/ArrayHelper.php
+++ src/rnd/helpers/ArrayHelper.php
@@ -9,6 +9,62 @@ use rnd\base\InvalidParamException;
 class ArrayHelper
 {
 	/**
+	 * Builds a map (key-value pairs) from a multidimensional array or an array of objects.
+	 * The `$from` and `$to` parameters specify the key names or property names to set up the map.
+	 * Optionally, one can further group the map according to a grouping field `$group`.
+	 *
+	 * For example,
+	 *
+	 * ```php
+	 * $array = [
+	 *     ['id' => '123', 'name' => 'aaa', 'class' => 'x'],
+	 *     ['id' => '124', 'name' => 'bbb', 'class' => 'x'],
+	 *     ['id' => '345', 'name' => 'ccc', 'class' => 'y'],
+	 * ];
+	 *
+	 * $result = ArrayHelper::map($array, 'id', 'name');
+	 * // the result is:
+	 * // [
+	 * //     '123' => 'aaa',
+	 * //     '124' => 'bbb',
+	 * //     '345' => 'ccc',
+	 * // ]
+	 *
+	 * $result = ArrayHelper::map($array, 'id', 'name', 'class');
+	 * // the result is:
+	 * // [
+	 * //     'x' => [
+	 * //         '123' => 'aaa',
+	 * //         '124' => 'bbb',
+	 * //     ],
+	 * //     'y' => [
+	 * //         '345' => 'ccc',
+	 * //     ],
+	 * // ]
+	 * ```
+	 *
+	 * @param array $array
+	 * @param string|\Closure $from
+	 * @param string|\Closure $to
+	 * @param string|\Closure $group
+	 * @return array
+	 */
+	public static function map($array, $from, $to, $group = null)
+	{
+		$result = [];
+		foreach ($array as $element) {
+			$key = static::getValue($element, $from);
+			$value = static::getValue($element, $to);
+			if ($group !== null) {
+				$result[static::getValue($element, $group)][$key] = $value;
+			} else {
+				$result[$key] = $value;
+			}
+		}
+
+		return $result;
+	}
+	/**
 	 * Check whether an array or [[\Traversable]] contains an element.
 	 *
 	 * This method does the same as the PHP function [in_array()](http://php.net/manual/en/function.in-array.php)
