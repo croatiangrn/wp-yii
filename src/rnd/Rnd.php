4633328768f9e4391bb37fe28d21a2c8b7a010f2
Marko Mikulic <mikulic.marko@spark.ba>
2017-08-14 10:34:37 +0200

    Add dump&die (dd) function

--- src/rnd/Rnd.php
+++ src/rnd/Rnd.php
@@ -7,3 +7,12 @@ class Rnd extends \rnd\BaseRnd {
 }
 //Rnd::$classMap = require(__DIR__ . '/../../vendor/autoload.php');
 Rnd::$container = new rnd\di\Container();
+
+if (!function_exists('dd')) {
+	function dd()
+	{
+		$args = func_get_args();
+		call_user_func_array('var_dump', $args);
+		die();
+	}
+}
